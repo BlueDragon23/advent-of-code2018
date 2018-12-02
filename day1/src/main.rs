81f4acb56876fb93e7a0022afb4613d06ac81d9d
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2018-12-02 16:40:15 +1000

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,26 @@
 use std::io::BufRead;
 use std::fs::File;
 use std::io::BufReader;
+use std::collections::HashSet;
 
 fn main() {
     let f = File::open("input/1.txt").unwrap();
     let reader = BufReader::new(f);
-    println!("{}", reader
+    let mut set = HashSet::new();
+    let numbers = reader
         .lines()
         .map(|line| line.unwrap())
-        .fold(0, |acc, x| acc + x.parse::<i32>().unwrap())
-    );
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    let mut acc: i32 = 0;
+    let mut index = 0;
+    loop {
+        acc += numbers[index];
+        index = (index + 1) % numbers.len();
+        if set.contains(&acc) {
+            println!("{}", acc);
+            return;
+        }
+        set.insert(acc);
+    }
 }
