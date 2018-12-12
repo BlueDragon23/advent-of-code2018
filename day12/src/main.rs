ee77038ea07fc7edde565256ba533dcd20c0c0b8
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2018-12-12 15:55:26 +1000

    Day 12 part 1

---
+++ day12/src/main.rs
@@ -0,0 +1,81 @@
+use std::io::BufRead;
+use std::fs::File;
+use std::io::BufReader;
+use std::collections::HashMap;
+
+fn main() {
+    let f = File::open("input/input.txt").unwrap();
+    let mut reader = BufReader::new(f);
+
+    let mut initial_state = String::new();
+    reader.read_line(&mut initial_state);
+
+    let mut initial = initial_state.trim_start_matches("initial state: ");
+    let mut state: Vec<_> = initial.chars().map(|c| if c == '#' { true } else { false } ).collect();
+    // Start at -200
+    let start_point = -40;
+    for _ in start_point..0 {
+        state.insert(0, false);
+    }
+    for _ in 0..30 {
+        state.push(false);
+    }
+
+    let mut _discard = String::new();
+    reader.read_line(&mut _discard);
+
+    let mut rules = HashMap::new();
+    reader
+        .lines()
+        .map(|line| line.unwrap())
+        .for_each(|line| {
+            let mut parts = line.split(" => ");
+            let key = parts.next().unwrap();
+            let mut key_num = 0;
+            let mut index = 4;
+            key.chars().for_each(|c| {
+                if c == '#' {
+                    key_num += i32::pow(2, index);
+                }
+                index -= 1;
+            });
+            let value = parts.next().unwrap();
+            let value_bool = match value {
+                "#" => true,
+                "." => false,
+                _ => panic!(),
+            };
+            rules.insert(key_num, value_bool);
+        });
+    println!("{}", state.iter().fold("0: ".to_owned(), |acc, b| if *b == true { acc + "#" } else { acc + "." }));
+    
+    for i in 0..20 {
+        let mut index = 0;
+        state = state.iter().skip(2).map(|b| {
+            let mut j = 4;
+            let key = state.iter().skip(index).take(5).fold(0, |acc, prev_bool| {
+                let mut next = acc;
+                if *prev_bool == true {
+                    next = acc + i32::pow(2, j);
+                }
+                j -= 1;
+                next
+            });
+            index += 1;
+            *rules.get(&key).unwrap()
+        }).collect();
+        state.insert(0, false);
+        state.insert(0, false);
+        println!("{}", state.iter().fold((i + 1).to_string() + ": ", |acc, b| if *b == true { acc + "#" } else { acc + "." }));
+    }
+    let mut index = start_point;
+    let result = state.iter().fold(0, |acc, b| {
+        let mut next = acc;
+        if *b == true {
+            next += index;
+        }
+        index += 1;
+        next
+    });
+    println!("{}", result);
+}
